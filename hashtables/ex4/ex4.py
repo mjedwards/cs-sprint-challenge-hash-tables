42dbb154359dc0a060b351064b13d77f9a5eef49
Michael Edwards <blumarvel@Michaels-MacBook-Air.local>
2020-10-12 20:47:06 -0400

    attempted third problem

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,21 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    num_dict = {}
+    stored = []
+    value = []
+    for i in range(len(a)):
+        if a[i] < 0:
+            stored.append(abs(a[i]))
+            
+            for j in range(len(stored)):
+                if stored[j] in a[j]:
+                    value.append(stored[j])
+
+                    result = value
+            
+
+        num_dict[a[i]] = a[i]
 
     return result
 
